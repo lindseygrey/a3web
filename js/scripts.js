a2cb88ea120cfdd6dd4891e924d43c5625d2cb87
perrydo <perrydo@flemingcollege.ca>
2021-11-21 15:43:19 -0500

    slideshow test

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,17 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// Perry code starts here
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 5000);
+
+// Perry code ends here
